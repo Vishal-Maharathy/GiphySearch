d2433b6339d5421e15fc40d17027e39f82dbd619
Vishal Maharathy <vishalmaharathy76@gmail.com>
2022-07-05 01:34:05 +0530

    users can now see loading after searching for gifs

--- assets/js/soundcloud.js
+++ assets/js/soundcloud.js
@@ -1,18 +1,30 @@
 let imgDiv = (link)=>{return `<img src="${link}" class="gifs-giphy" alt="">`}
+let noResults = ()=>{return `<div id="text-inside-container">No Results Found</div>`}
 
-let searhcGiphy = $('#searchGiphy');
-searhcGiphy[0].addEventListener('keypress', async (e)=>{
+let searchGiphy = $('#searchGiphy');
+searchGiphy[0].addEventListener('keypress', async (e)=>{
     if(e.key==='Enter'){
+        let outerContainer = $('#container-media-outer')
         let gifContainer = $('#giphy-imgs-gifs');
-        gifContainer[0].innerHTML = ''
-        let key = searhcGiphy[0].value;
-        searhcGiphy[0].value = ''
+        gifContainer[0].innerHTML = '<div id="text-inside-container">Loading....</div>'
+        let key = searchGiphy[0].value;
+        searchGiphy[0].value = ''
         let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=5GQf66UnALROIea3kfUwLmzpgeuMpV3g&q=${key}&limit=30&offset=0&rating=g&lang=en`)
         let data = await response.json();
-        for(let i of data.data){
-            // prepend 
-            gifContainer[0].innerHTML+=imgDiv(i.images.fixed_width.url)
-            // console.log(i.images.fixed_width.url);
+        gifContainer[0].innerHTML = ''
+        if(!data.data.length){
+            outerContainer[0].style.height = '40px'
+            gifContainer[0].style.height = '40px'
+            gifContainer[0].innerHTML = noResults()
+            return;
         }
+        else{
+            outerContainer[0].style.height = '100px'
+            gifContainer[0].style.height = '80%'
+            for(let i of data.data){
+                gifContainer[0].innerHTML+=imgDiv(i.images.fixed_width.url)
+            }
+        }
+        
     }
 })
